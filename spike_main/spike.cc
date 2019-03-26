e5ba5a50ec3e2913fd8133d1186e8b4ab646f367
Tim Newsome <tim@sifive.com>
2019-03-26 13:20:50 -0700

    Add --without-hasel argument for testing.

--- spike_main/spike.cc
+++ spike_main/spike.cc
@@ -46,6 +46,7 @@ static void help()
       "required for a DMI access [default 0]\n");
   fprintf(stderr, "  --abstract-rti=<n>    Number of Run-Test/Idle cycles "
       "required for an abstract command to execute [default 0]\n");
+  fprintf(stderr, "  --without-hasel       Debug module supports hasel\n");
   exit(1);
 }
 
@@ -104,6 +105,7 @@ int main(int argc, char** argv)
   bool require_authentication = false;
   unsigned dmi_rti = 0;
   unsigned abstract_rti = 0;
+  bool support_hasel = true;
   std::vector<int> hartids;
 
   auto const hartids_parser = [&](const char *s) {
@@ -155,6 +157,8 @@ int main(int argc, char** argv)
       [&](const char* s){dmi_rti = atoi(s);});
   parser.option(0, "abstract-rti", 1,
       [&](const char* s){abstract_rti = atoi(s);});
+  parser.option(0, "without-hasel", 0,
+      [&](const char* s){support_hasel = false;});
 
   auto argv1 = parser.parse(argv);
   std::vector<std::string> htif_args(argv1, (const char*const*)argv + argc);
@@ -166,7 +170,7 @@ int main(int argc, char** argv)
 
   sim_t s(isa, nprocs, halted, start_pc, mems, htif_args, std::move(hartids),
       progsize, max_bus_master_bits, require_authentication,
-      abstract_rti);
+      abstract_rti, support_hasel);
   std::unique_ptr<remote_bitbang_t> remote_bitbang((remote_bitbang_t *) NULL);
   std::unique_ptr<jtag_dtm_t> jtag_dtm(
       new jtag_dtm_t(&s.debug_module, dmi_rti));
